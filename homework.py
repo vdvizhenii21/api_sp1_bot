72669a19982cd630b28ccd666013fda34989dcd2
Vitalii Tsyoma <vitaliytsyoma@icloud.com>
2021-05-06 13:45:43 +0300

    third

--- homework.py
+++ homework.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
